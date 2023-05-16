98271e489d5b677071f2cc0fd294f3c62a615d3b
yyang <dayatang@gmail.com>
2023-05-16 09:21:36 +0800

    转变为kotlin

--- src/main/kotlin/io/codera/quant/Application.kt
+++ src/main/kotlin/io/codera/quant/Application.kt
@@ -5,6 +5,7 @@ import com.google.inject.Guice
 import io.codera.quant.config.Config
 import io.codera.quant.strategy.Strategy
 import io.codera.quant.strategy.StrategyRunner
+import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.CommandLineParser
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
@@ -18,36 +19,41 @@ object Application {
     private val logger = LoggerFactory.getLogger(Application::class.java)
     @JvmStatic
     fun main(args: Array<String>) {
-        val options = Options()
-        options.addOption("h", true, "Interactive Brokers host")
-        options.addOption("p", true, "Interactive Brokers port")
-        options.addOption("l", true, "List of symbols to trade")
-
-        // create the parser
-        val parser: CommandLineParser = DefaultParser()
+        logger.info("Starting app...")
         try {
-            // parse the command line arguments
-            val cmd = parser.parse(options, args)
-            logger.info("Starting app")
-            Preconditions.checkState(cmd.getOptionValue("h") != null, "host can not be null")
-            Preconditions.checkState(cmd.getOptionValue("p") != null, "port can not be null")
-            Preconditions.checkState(cmd.getOptionValue("l") != null, "symbol can not be null")
+            val cmd = getCmd(args)
             val injector = Guice.createInjector(
                 Config(
                     cmd.getOptionValue("h"),
-                    Integer.valueOf(cmd.getOptionValue("p")),
+                    cmd.getOptionValue("p").toInt(),
                     cmd.getOptionValue("l")
                 )
             )
             val strategyRunner = injector.getInstance(StrategyRunner::class.java)
-            val strategy = injector.getInstance(
-                Strategy::class.java
-            )
-            val symbolList = cmd.getOptionValue("l").split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            strategyRunner.run(strategy, Arrays.asList(*symbolList))
+            val strategy = injector.getInstance(Strategy::class.java)
+            val symbolList = cmd.getOptionValue("l")
+                .split(",".toRegex())
+                .dropLastWhile(String::isEmpty)
+                .toTypedArray()
+            strategyRunner.run(strategy, listOf(*symbolList))
         } catch (e: Exception) {
             // oops, something went wrong
             logger.error("Something went wrong", e)
         }
+    }
+
+    private fun getCmd(args: Array<String>): CommandLine {
+        // parse the command line arguments
+        val options = Options()
+        options.addOption("h", true, "Interactive Brokers host")
+        options.addOption("p", true, "Interactive Brokers port")
+        options.addOption("l", true, "List of symbols to trade")
+        // create the parser
+        val parser: CommandLineParser = DefaultParser()
+        val cmd = parser.parse(options, args)
+        require(cmd.getOptionValue("h") != null) {"host can not be null"}
+        require(cmd.getOptionValue("p") != null) {"port can not be null"}
+        require(cmd.getOptionValue("l") != null) {"symbol can not be null"}
+        return cmd
     }
 }
